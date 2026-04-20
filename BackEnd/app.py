7783cf258952549b11c4494a699275f95f80a9a2
Aayush <aayushjindal65@gmail.com>
2026-04-20 11:18:12 +0530

    Updated CSS

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -7,7 +7,9 @@ CORS(app)
 # Database
 all_movies = [
     {"id": 1, "title": "Inception", "thumbnail": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQovCe0H45fWwAtV31ajOdXRPTxSsMQgPIQ3lcZX_mAW0jXV3kH", "rating": 4.8},
-    {"id": 2, "title": "Interstellar", "thumbnail": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9oW0XQlu1lo1G_49M-YwGzKR6rUg-CtflZj07HfbT8d2GwKWg", "rating": 4.9}
+    {"id": 2, "title": "Interstellar", "thumbnail": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9oW0XQlu1lo1G_49M-YwGzKR6rUg-CtflZj07HfbT8d2GwKWg", "rating": 4.9},
+    {"id": 3, "title": "The Dark Knight", "thumbnail": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQkUywIUXDjHSQJIaNHYVs08osgBpF5Ot-xmB_omyEZeeRP9Xug", "rating": 4.7},
+    {"id": 4, "title": "One Peice", "thumbnail": "/Users/aayushjindal/Documents/Web-Dev-Project/Storage/MV5BMTNjNGU4NTUtYmVjMy00YjRiLTkxMWUtNzZkMDNiYjZhNmViXkEyXkFqcGc@._V1_.jpg", "rating": 4.9},
 ]
 my_watchlist = []
 
@@ -23,7 +25,7 @@ def add_to_list():
     data = request.json
     my_watchlist.append(data)
     print(f"Watchlist updated: {my_watchlist}")
-    return jsonify({"message": f"{data['title']} added to watchlist!"}), 201
+    return jsonify({"message": f"{data['title']} Added to watchlist!"}), 201
 
 if __name__ == "__main__":
     app.run(port=5001, debug=True)
