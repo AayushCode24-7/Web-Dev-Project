4a983ce91682fade51ff75a1422d08286df46d0a
Aayush <aayushjindal65@gmail.com>
2026-05-08 13:31:01 +0530

    Update app.py

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -91,4 +91,4 @@ def search_movies():
 if __name__ == "__main__":
     # Use the port 5003
     port = int(os.environ.get("PORT", 5003))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug = True)
